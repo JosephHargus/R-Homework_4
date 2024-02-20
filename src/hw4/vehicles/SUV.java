5a9d4112fce93547140226a0bdb91f094f58da96
riannaellis <riannaellis@pcp003512pcs.dhcp.missouri.edu>
2024-02-20 11:25:46 -0600

    Last SUV subclass commit did not work correctly
    For some reason, the last commit only committed the creation of the
    class, and did not include the constructor, copy constructor,
    or the overridden calculateMaintenanceCost(),
    calculateFuelEffficiency(), and startEngine() methods. This commit
    contains the rest of the changes.

--- src/hw4/vehicles/SUV.java
+++ src/hw4/vehicles/SUV.java
@@ -1,5 +1,34 @@
 package hw4.vehicles;
 
-public class SUV {
+public class SUV extends Vehicle{
+
+	//constructor
+	public SUV(String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType,
+			double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
+		super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
+	}
+	
+	//copy constructor
+	public SUV(SUV suv) {
+		super(suv);
+	}
+
+	//overridden methods
+	@Override
+	public double calculateMaintenanceCost(double distance) {
+		double maintenanceCost = distance * mass  * (2024 - modelYear) * cylinders * 0.001;
+		return maintenanceCost;
+	}
+
+	@Override
+	public double calculateFuelEfficiency(double distance, double fuelPrice) {
+		double fuelEfficiency = cylinders *  gasTankCapacity * fuelPrice /  distance * 0.05;
+		return fuelEfficiency;
+	}
+
+	@Override
+	public void startEngine() {
+		System.out.println("The SUV engine starts using PUSHSTART");
+	}
 
 }
